c4c9dc3a879ce826df854defd8a86e73fee4eeab
Christopher-Carey <ccarey40@gmail.com>
2019-11-21 13:43:56 -0600

    Worked on Game Play Page and Game Play js

---
+++ wwwroot/js/GamePlay.js
@@ -0,0 +1,35 @@
+$('#P1Char1').click(function () {
+    $('#P1Fight').prop('src', "/images/1.png");
+    
+});
+$('#P1Char2').click(function () {
+    $('#P1Fight').prop('src', "/images/2.png");
+});
+$('#P1Char3').click(function () {
+    $('#P1Fight').prop('src', "/images/3.png");
+});
+$('#P1Char4').click(function () {
+    $('#P1Fight').prop('src', "/images/4.png");
+});
+$('#P1Char5').click(function () {
+    $('#P1Fight').prop('src', "/images/5.png");
+});
+
+
+// PLAYER TWO
+// ----------------------------
+$('#P2Char1').click(function () {
+    $('#P2Fight').prop('src', "/images/1.png");
+});
+$('#P2Char2').click(function () {
+    $('#P2Fight').prop('src', "/images/2.png");
+});
+$('#P2Char3').click(function () {
+    $('#P2Fight').prop('src', "/images/3.png");
+});
+$('#P2Char4').click(function () {
+    $('#P2Fight').prop('src', "/images/4.png");
+});
+$('#P2Char5').click(function () {
+    $('#P2Fight').prop('src', "/images/5.png");
+});
